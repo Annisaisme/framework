91268381fbac340b5e641f535a5290e47b882b00
annisaisme <annisasalsabilla112@gmail.com>
2023-05-09 21:42:51 +0700

    first commit

---
+++ app/Http/Controllers/RouteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RouteController extends Controller
+{
+    public function index() {
+        return "This is from Controller";
+    }
+    public function profile($profileId) {
+        return "This is Profile from Controller, profile id: ".$profileId;
+    }
+}
